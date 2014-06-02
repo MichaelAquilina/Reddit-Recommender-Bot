5369987a4c5dfad58b67d7b7075f55547fd109ae
Michael Aquilina <michaelaquilina@gmail.com>
2014-06-02 16:30:58 +0100

    Ensure the out parameter is a directory

--- post_extractor.py
+++ post_extractor.py
@@ -21,50 +21,57 @@ if __name__ == '__main__':
     if not os.path.exists(args.out):
         os.mkdir(args.out)
 
-    # Amount of requests required based on the limit specified
-    count = args.limit
-    i = 0
+    if not os.path.isdir(args.out):
+        print 'Out parameter must be a directory'
+    else:
+        # Amount of requests required based on the limit specified
+        count = args.limit
+        i = 0
 
-    # After pointer for retrieving next batch of submissions
-    after = None
+        # After pointer for retrieving next batch of submissions
+        after = None
 
-    while count:
+        while count:
 
-        # Request data
-        params = {
-            'limit': min(count, MAX_LIMIT),
-            'after': after,
-            'show': 'all',
-            't': args.period,
-        }
+            # Request data
+            params = {
+                'limit': min(count, MAX_LIMIT),
+                'after': after,
+                'show': 'all',
+                't': args.period,
+            }
 
-        # Request top posts in JSON format
-        r = requests.get(
-            'http://www.reddit.com/r/{}/top.json'.format(args.subreddit),
-            headers={'user-agent': 'postextractor'},
-            params=params
-        )
+            # Request top posts in JSON format
+            r = requests.get(
+                'http://www.reddit.com/r/{}/top.json'.format(args.subreddit),
+                headers={'user-agent': 'postextractor'},
+                params=params
+            )
 
-        if r.ok:
-            submission_data = r.json()
+            if r.ok:
+                submission_data = r.json()
 
-            # Detect if no data is returned before continuing
-            if len(submission_data['data']['children']) == 0:
-                print 'The subreddit \'{}\' does not exist'.format(args.subreddit)
+                # Detect if no data is returned before continuing
+                if len(submission_data['data']['children']) == 0:
+                    print 'The subreddit \'{}\' does not exist'.format(args.subreddit)
+                    break
+
+                for post in submission_data['data']['children']:
+                    print post['data']['title']
+
+                    # This should be threaded!
+                    if not post['data']['is_self']:
+                        download_page(post['data']['url'])
+
+                after = submission_data['data']['after']
+                count -= len(submission_data['data']['children'])
+
+                save_path = os.path.join(args.out, '{}.{}.json'.format(args.subreddit, i))
+
+                with open(save_path, 'w') as f:
+                    json.dump(submission_data, f, indent=4)
+
+                i += 1
+            else:
+                print 'An Error occurred'
                 break
-
-            for post in submission_data['data']['children']:
-                print post['data']['title']
-
-            after = submission_data['data']['after']
-            count -= len(submission_data['data']['children'])
-
-            save_path = os.path.join(args.out, '{}.{}.json'.format(args.subreddit, i))
-
-            with open(save_path, 'w') as f:
-                json.dump(submission_data, f, indent=4)
-
-            i += 1
-        else:
-            print 'An Error occurred'
-            break
