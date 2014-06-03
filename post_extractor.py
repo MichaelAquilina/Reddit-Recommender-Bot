e574e4c1b22916788d87ce6aeaab308ce4c55dc7
Michael Aquilina <michaelaquilina@gmail.com>
2014-06-03 13:50:34 +0100

    Change default period value to 'year'

--- post_extractor.py
+++ post_extractor.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
     parser.add_argument('subreddit', type=str, help='Name of the subreddit to retrieve posts from')
     parser.add_argument('--limit', type=int, default=25, help='Number of submissions to retrieve')
     parser.add_argument('--out', type=str, default='', help='Path to store incoming JSON files')
-    parser.add_argument('--period', choices=('year', 'month', 'week', 'all'), default='all')
+    parser.add_argument('--period', choices=('year', 'month', 'week', 'all'), default='year')
     parser.add_argument('--threads', type=int, default=10)
 
     args = parser.parse_args()
