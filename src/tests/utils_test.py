97c61d4d911ce69f6099d89f65ccea1587dc4128
Michael Aquilina <michaelaquilina@gmail.com>
2014-07-11 01:32:35 +0200

    Add relative assertion for recursive search_files test case

--- src/tests/utils_test.py
+++ src/tests/utils_test.py
@@ -21,15 +21,19 @@ def test_search_files():
 
 
 def test_search_files_recursive():
-    expected = []
+    expected_abs = []
+    expected_rel = []
+
     tempdir = tempfile.mkdtemp()
     current = tempdir
 
     for i in xrange(10):
         tf = tempfile.mktemp(suffix=str(i), dir=current)
-        expected.append(tf)
+        expected_abs.append(tf)
+        expected_rel.append(os.path.relpath(tf, tempdir))
         with open(tf, 'w') as fp:
             fp.write('Temporary Data')
         current = tempfile.mkdtemp(dir=current)
 
-    assert generator_unordered_cmp(search_files(tempdir), expected)
+    assert generator_unordered_cmp(search_files(tempdir), expected_abs)
+    assert generator_unordered_cmp(search_files(tempdir, relative=True), expected_rel)
