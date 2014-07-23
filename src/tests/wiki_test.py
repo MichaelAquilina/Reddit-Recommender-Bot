2af652de95384ad9f507d3a3640f76b367f8a65c
Michael Aquilina <michaelaquilina@gmail.com>
2014-07-23 11:47:44 +0200

    Reorder test sequences for _re_link_pattern test

--- src/tests/wiki_test.py
+++ src/tests/wiki_test.py
@@ -4,10 +4,10 @@ from wiki import _re_link_pattern
 
 
 def test_link_pattern_test():
-    assert _re_link_pattern.findall(u'[[Göta Canal]]') == [u'Göta Canal']
     assert _re_link_pattern.findall('\'\'[[Apple Inc.]]\'\' and [[Iphone 4|That phone]]') == ['Apple Inc.', 'Iphone 4']
     assert _re_link_pattern.findall('[[The 1st, Avengers]]') == ['The 1st, Avengers']
     assert _re_link_pattern.findall('Performing some [[C* Algebra]] in [[Debian!/Ubuntu?]]!') == ['C* Algebra', 'Debian!/Ubuntu?']
     assert _re_link_pattern.findall('[["Loving" - \'it\']] with [[C++#functions|C plus plus]]') == ['"Loving" - \'it\'', 'C++']
     assert _re_link_pattern.findall('[[Queen (band)|Queen]]') == ['Queen (band)']
     assert _re_link_pattern.findall('[[Target page#Target section|display text]]') == ['Target page']
+    assert _re_link_pattern.findall(u'[[Göta Canal]]') == [u'Göta Canal']
