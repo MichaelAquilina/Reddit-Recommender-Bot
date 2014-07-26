8f8149e706931c598797c158699adca3417cfc7d
Michael Aquilina <michaelaquilina@gmail.com>
2014-07-26 14:07:36 +0200

    Add secondary SSCursor object to WikiIndex to be used when it makes sense

--- src/wikiindex.py
+++ src/wikiindex.py
@@ -43,7 +43,8 @@ class WikiIndex(object):
 
     def __init__(self, user, passwd, host, db):
         self.connection = MySQLdb.connect(user=user, passwd=passwd, host=host, db=db)
-        self.cur = self.connection.cursor(cursorclass=MySQLdb.cursors.Cursor)
+        self._cur = self.connection.cursor(cursorclass=MySQLdb.cursors.Cursor)
+        self._sscur = self.connection.cursor(cursorclass=MySQLdb.cursors.SSCursor)
         self._db = db
         self._host = host
 
@@ -51,7 +52,8 @@ class WikiIndex(object):
         return '<WikiIndex %s@%s>' % (self._db, self._host)
 
     def close(self):
-        self.cur.close()
+        self._cur.close()
+        self._sscur.close()
         self.connection.close()
 
     def get_document_frequencies(self, term_id_list):
@@ -63,13 +65,13 @@ class WikiIndex(object):
             var_string += u'%d,' % tid
         var_string = var_string[:-1]
 
-        self.cur.execute("""
+        self._cur.execute("""
             SELECT TermID, COUNT(*)
             FROM TermOccurrences
             WHERE TermID IN (%s)
             GROUP BY TermID
         """ % var_string)
-        return self.cur.fetchall()
+        return self._cur.fetchall()
 
     def get_term_ids(self, term_name_list):
         """
@@ -80,12 +82,12 @@ class WikiIndex(object):
             var_string += u'\'%s\',' % term_name
         var_string = var_string[:-1]
 
-        self.cur.execute("""
+        self._cur.execute("""
             SELECT TermName, TermID
             FROM Terms
             WHERE TermName IN (%s);
         """ % var_string)
-        return self.cur.fetchall()
+        return self._cur.fetchall()
 
     def get_page_ids(self, page_name_list):
         """
@@ -96,25 +98,25 @@ class WikiIndex(object):
             var_string += u'\'%s\',' % term_name
         var_string = var_string[:-1]
 
-        self.cur.execute("""
+        self._cur.execute("""
             SELECT PageName, PageID
             FROM Pages
             WHERE PageName IN (%s);
         """ % var_string)
-        return self.cur.fetchall()
+        return self._cur.fetchall()
 
     def get_page_links(self, page_id):
         """
         Returns a list of (TargetPageID, TargetPageName, LinkCounter)
         """
-        self.cur.execute("""
+        self._cur.execute("""
             SELECT T2.PageID, T2.PageName, T1.Counter
             FROM PageLinks T1
             INNER JOIN Pages T2 ON T1.TargetPageID = T2.PageID
             WHERE T1.PageID = %s;
         """, (page_id, ))
 
-        return self.cur.fetchall()
+        return self._cur.fetchall()
 
     def get_documents(self, term_id, min_counter=2, limit=200):
         """
@@ -125,23 +127,23 @@ class WikiIndex(object):
         """
         # Ordering by counter is super duper slow due to lack of index
         # and large table size (needs to perform a filesort!)
-        self.cur.execute("""
+        self._cur.execute("""
             SELECT T1.PageID, T1.PageName, T2.Counter
             FROM Pages AS T1
             INNER JOIN TermOccurrences AS T2 ON T1.PageID = T2.PageID
             WHERE TermID = %s AND Counter > %s
             ORDER BY Counter DESC
-            LIMIT %s
+            LIMIT %s;
         """, (term_id, min_counter, limit))
-        return self.cur.fetchall()
+        return self._cur.fetchall()
 
     def get_corpus_size(self):
         """
         Returns the size of the corpus which excludes all unprocessed pages.
         """
-        self.cur.execute('SELECT COUNT(*) FROM Pages WHERE Processed=1;')
-        (corpus, ) = self.cur.fetchone()
-        self.cur.fetchall()
+        self._cur.execute('SELECT Size FROM CorpusSize;')
+        (corpus, ) = self._cur.fetchone()
+        self._cur.fetchall()
 
         return corpus
 
