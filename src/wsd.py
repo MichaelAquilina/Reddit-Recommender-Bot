78c86b9201c689e49f175345556930b1ce8afb37
Michael Aquilina <michaelaquilina@gmail.com>
2014-07-28 12:51:41 +0200

    Fix wsd.py to work with WikiIndex and add another test case

--- src/wsd.py
+++ src/wsd.py
@@ -34,12 +34,23 @@ if __name__ == '__main__':
     print(wiki)
 
     t0 = time.time()
-    result = wiki.word_concepts("""
+    query_vector, terms, results = wiki.word_concepts("""
         The original open source software “Benevolent Dictator For Life” and author of Python, Guido van Rossum,
         is leaving Google to join Dropbox, the startup will announce later today. Van Rossum was a software
         engineer at Google since 2005, and should be a huge help as Dropbox is built on Python. He’s the latest
         big hire by the cloud storage startup that’s capitalizing on its 100 million-user milestone.
     """)
+
+    print(results[:20])
+
+    query_vector, terms, results = wiki.word_concepts(""""
+        While Valve and its Steam distribution platform have been pushing Linux as the future of PC gaming for a
+        long while now, the folks at online store GOG have contented themselves with PC and Mac software. That
+        situation changed today, as GOG (formerly Good Old Games) announced support for Linux, offering over 50
+        titles for DRM-free download.
+    """)
+
+    print(results[:20])
 
     wiki.close()
 
