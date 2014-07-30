327033740ae304e04041b8ee3889d89c1bf2b23e
Michael Aquilina <michaelaquilina@gmail.com>
2014-07-30 10:53:28 +0200

    Ignore WikiMedia articles

--- src/wiki.py
+++ src/wiki.py
@@ -26,7 +26,7 @@ MIN_PAGE_LENGTH = 200  # Minimum Page Length in terms
 # large without appropriate compression)
 
 # List of Page titles to ignore when they have the following prefix
-IGNORE_LIST = ('List of', 'Wikipedia:', 'Template:', 'File:', 'Category:', 'Help:', 'Portal:')
+IGNORE_LIST = ('List of', 'MediaWiki:', 'Wikipedia:', 'Template:', 'File:', 'Category:', 'Help:', 'Portal:')
 
 # Wikipedia inner link pattern
 # http://en.wikipedia.org/wiki/Wikipedia:Tutorial/Wikipedia_links
