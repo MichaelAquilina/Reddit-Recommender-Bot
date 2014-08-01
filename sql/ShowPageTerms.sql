ddc31815105b8b77cec8dcc3e479755c11256c77
Michael Aquilina <michaelaquilina@gmail.com>
2014-08-01 21:11:42 +0200

    ShowPageTerms sql procedure now also includes tfidf results

--- sql/ShowPageTerms.sql
+++ sql/ShowPageTerms.sql
@@ -4,10 +4,12 @@ DELIMITER //
 
 CREATE PROCEDURE ShowPageTerms(TargetPage VARCHAR(150))
 BEGIN
-	SELECT T1.PageID, T2.PageName, T1.TermID, T3.TermName, T1.Counter AS TF
+	SELECT T1.PageID, T2.PageName, T1.TermID, T4.TermName, T1.Counter AS Tf, DocumentFrequency AS Df, T3.Tfidf
 	FROM TermOccurrences T1
-	INNER JOIN Pages T2 ON T1.PageID = T2.PageID
-	INNER JOIN Terms T3 ON T1.TermID = T3.TermID
+	INNER JOIN Pages T2 ON T2.PageID = T1.PageID
+	INNER JOIN TfidfValues T3 ON T3.PageID = T1.PageID AND T3.TermID = T1.TermID
+	INNER JOIN Terms T4 ON T4.TermID = T1.TermID
+	INNER JOIN DocumentFrequencies T5 ON T5.TermID = T1.TermID
 	WHERE PageName = TargetPage
-	ORDER BY Counter DESC;
+	ORDER BY Tfidf DESC;
 END//
