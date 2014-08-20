88493ec13b544157abe77dac69070aac16d10812
Michael Aquilina <michaelaquilina@gmail.com>
2014-08-21 00:12:12 +0100

    Plot Document Frequency rather than Term Frequency

--- tests/reddit_term_frequency.py
+++ tests/reddit_term_frequency.py
@@ -7,6 +7,7 @@ from goose import Goose, Configuration
 
 from textparser import word_tokenize
 from datasource import load_data_source
+from index.hashedindex import HashedIndex
 
 data_path = '/home/michaela/Development/Reddit-Testing-Data'
 subreddit_list = ['python', 'science', 'programming', 'technology']
@@ -26,24 +27,36 @@ for subreddit_index, subreddit in enumerate(subreddit_list):
     config.use_meta_language = False
     goose = Goose(config)
 
+    term_index = HashedIndex()
+
+    if os.path.exists('%s.json.bz2' % subreddit):
+        print('Found a JSON encoded index file for %s' % subreddit)
+        term_index.load('%s.json.bz2' % subreddit, compressed=True)
+    else:
+        for page_index, path in enumerate(load_data_source(data_path, subreddit, 0)):
+            abs_path = os.path.join(pages_path, path)
+            if os.path.exists(abs_path):
+                print '(%d) %d: %s' % (subreddit_index, page_index, path)
+
+                with open(abs_path, 'r') as fp:
+                    html_text = fp.read()
+
+                article = goose.extract(raw_html=html_text)
+
+                for term in word_tokenize(article.cleaned_text, remove_urls=True):
+                    term_index.add_term_occurrence(term, path)
+
+        print('Saving for future pre-loading...')
+        term_index.save('%s.json.bz2' % subreddit, compressed=True)
+
+    # Document Frequency is more relevant than Term Frequency for the task at hand
     term_counter = Counter()
+    for term in term_index.terms():
+        term_counter[term] = term_index.get_document_frequency(term)
 
-    for page_index, path in enumerate(load_data_source(data_path, subreddit, 0)):
-        abs_path = os.path.join(pages_path, path)
-        if os.path.exists(abs_path):
-            print '(%d) %d: %s' % (subreddit_index, page_index, path)
+    print(term_counter.most_common(20))
 
-            with open(abs_path, 'r') as fp:
-                html_text = fp.read()
-
-            article = goose.extract(raw_html=html_text)
-
-            for term in word_tokenize(article.cleaned_text, remove_urls=True):
-                term_counter[term] += 1
-
-    print term_counter.most_common(20)
-
-    n = 1500
+    n = min(len(term_counter), 5000)
 
     axes[i, j].set_xlim(0, n)
     axes[i, j].set_title(subreddit)
