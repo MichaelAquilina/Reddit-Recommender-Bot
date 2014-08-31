85278bf68d9f8698527011eb8334a2cde376c9e9
Michael Aquilina <michaelaquilina@gmail.com>
2014-08-31 01:30:45 +0100

    zipfian.py is now also plotted on a log log scale

--- figures/zipfian.py
+++ figures/zipfian.py
@@ -1,4 +1,6 @@
-from nltk.corpus import inaugural as corpus
+from __future__ import division
+
+from nltk.corpus import brown as corpus
 from matplotlib import pyplot as plt
 from collections import Counter
 
@@ -6,15 +8,22 @@ word_counter = Counter()
 for word in corpus.words():
     word_counter[word.lower()] += 1
 
+print(word_counter.most_common(20))
 print('%d Unique Terms' % len(word_counter))
 
+n = len(word_counter)
+
 # Generate the coordinates for plotting
-x = range(len(word_counter))
 y = [b for (a, b) in word_counter.most_common()]
 
-ax = plt.axes(xlim=(0, 800), ylim=(0, 2500), xmargin=0, ymargin=0)
+plt.scatter(range(n), y, edgecolors='none')
+plt.plot(range(1, n), [y[0] / x for x in range(1, n)], color='red', linewidth='2.0', label='1/x')
 
-ax.fill_between(x, y)
+plt.yscale('log')
+plt.xscale('log')
+
+plt.xlabel('rank')
+plt.ylabel('term frequency')
 
 plt.tight_layout()
 plt.show()
