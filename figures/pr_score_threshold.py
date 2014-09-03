e406cf94b70593619c4b41b6152bfa62df3a06b3
Michael Aquilina <michaelaquilina@gmail.com>
2014-09-03 22:32:09 +0100

    Use inbuilt sklearn precisio_recall_curve and roc_curve methods

--- figures/pr_score_threshold.py
+++ figures/pr_score_threshold.py
@@ -50,10 +50,8 @@ def evaluate_boc(data, n_folds, boc_params):
 
     kf = StratifiedKFold(label_vector, n_folds=n_folds)
 
-    print('Generating mean precision and recall using %d folds' % n_folds)
     scores = []
     y_test = []
-
     for index, (train, test) in enumerate(kf):
         classifier = SVC(kernel='linear', C=0.8)
         classifier.fit(feature_matrix[train], label_vector[train])
@@ -87,10 +85,8 @@ def evaluate_bow(data, n_folds):
 
     kf = StratifiedKFold(label_vector, n_folds=n_folds)
 
-    print('Generating mean precision and recall using %d folds' % n_folds)
     scores = []
     y_test = []
-
     for index, (train, test) in enumerate(kf):
         classifier = SVC(kernel='linear', C=0.8)
         classifier.fit(feature_matrix[train], label_vector[train])
@@ -102,21 +98,6 @@ def evaluate_bow(data, n_folds):
     y_test = np.concatenate(y_test)
 
     return scores, y_test
-
-
-def precision_recall_curve(threshold_range, y_scores, y_test, p_modifier=0, r_modifier=0):
-    x = []
-    y = []
-
-    for threshold in threshold_range:
-        y_pred = y_scores > threshold
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-
-        x.append(recall + r_modifier)
-        y.append(precision + p_modifier)
-
-    return x, y
 
 
 if __name__ == '__main__':
@@ -131,49 +112,53 @@ if __name__ == '__main__':
     # Load appropriate pages from data source
     data = load_data_source(data_path, subreddit, page_samples=600, seed=0, relative=False)
 
-    marker_params = {
-        'markeredgecolor': 'none',
-        'markersize': 8.0,
+    line_params = {
+        'linewidth': 2.0,
     }
 
     # Set up the precision recall graph
+    plt.figure(1)
     plt.title('Precision-Recall for \'%s\' subreddit' % subreddit)
     plt.xlabel('Recall')
     plt.ylabel('Precision')
+    plt.ylim(0.0, 1.05)
+    plt.xlim(0.01, 1.0)  # precision can be undefined for 0
 
-    thresholds = np.arange(-1.5, 4, 0.4)
+    # Set up the ROC graph
+    plt.figure(2)
+    plt.title('ROC curve for \'%s\' subreddit' % subreddit)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
 
     print('Evaluating Bag of Words')
     scores, actual = evaluate_bow(data, 4)
-    plt.plot(
-        *precision_recall_curve(thresholds, scores, actual, p_modifier=-0.02),
-        label='BoW', color='b', marker='o',
-        **marker_params
-    )
+    plt.figure(1)
+    precision, recall, _ = precision_recall_curve(actual, scores)
+    plt.plot(recall, precision - 0.02, label='BoW', **line_params)
+    plt.figure(2)
+    fpr, tpr, _ = roc_curve(actual, scores)
+    plt.plot(fpr, tpr, label='BoW', **line_params)
 
     print('Evaluating Bag of Concepts')
     scores, actual = evaluate_boc(data, 4, {'n': 20, 'r': 45})
-    plt.plot(
-        *precision_recall_curve(thresholds, scores, actual),
-        label='BoC', color='g', marker='v',
-        **marker_params
-    )
+    plt.figure(1)
+    precision, recall, _ = precision_recall_curve(actual, scores)
+    plt.plot(recall, precision, label='BoC', **line_params)
+    plt.figure(2)
+    fpr, tpr, _ = roc_curve(actual, scores)
+    plt.plot(fpr, tpr, label='BoC', **line_params)
 
     print('Evaluating Bag of Concepts 2')
-    scores, actual = evaluate_boc(data, 4, {'n': 20, 'r': 60})
-    plt.plot(
-        *precision_recall_curve(thresholds, scores, actual),
-        label='BoC 2', color='r', marker='D',
-        **marker_params
-    )
-
-    print('Evaluating Bag of Concepts 3')
     scores, actual = evaluate_boc(data, 4, {'n': 20, 'r': 30})
-    plt.plot(
-        *precision_recall_curve(thresholds, scores, actual),
-        label='BoC 3', color='m', marker='s',
-        **marker_params
-    )
+    plt.figure(1)
+    precision, recall, _ = precision_recall_curve(actual, scores)
+    plt.plot(recall, precision, label='BoC 2', **line_params)
+    plt.figure(2)
+    fpr, tpr, _ = roc_curve(actual, scores)
+    plt.plot(fpr, tpr, label='BoC 2', **line_params)
 
     plt.legend(loc='lower left')
+    plt.tight_layout()
     plt.show()
